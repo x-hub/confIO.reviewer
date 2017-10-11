264fa672e839840b7a380988b91c360e31d0f987
Amine Hakkou <amine.hakkou.pro@gmail.com>
2017-10-11 21:49:28 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View>
+        <Text>
+            Hello World
+        </Text>
+      </View>
+    );
+  }
+}
