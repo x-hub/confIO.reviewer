15708a84d62d6e04cc84f4856ed6bc34e0f2fb65
Bassi Hassan <bassihassan3@gmail.com>
2018-03-06 13:48:40 +0000

    Fix [ #41 ] : fix QrScan issue (#42)

--- src/App/index.js
+++ src/App/index.js
@@ -14,10 +14,30 @@ import Navigator from 'app/Navigator';
 import getTheme from 'nb-theme/components';
 import materialTheme from 'nb-theme/variables/material';
 import animations from 'shared/animations';
+import { PermissionsAndroid } from 'react-native';
 
 const theme = getTheme(materialTheme);
 
 export default class App extends Component {
+    async componentWillMount(){
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.CAMERA,
+                {
+                    'title': 'ConfIoReviewer App Camera Permission',
+                    'message': 'ConfIoReviewer App needs access to your camera ' +
+                    'so you can take awesome pictures.'
+                }
+            )
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log("You can use the camera")
+            } else {
+                console.log("Camera permission denied")
+            }
+        } catch (err) {
+            //
+        }
+    }
 
     render() {
 
